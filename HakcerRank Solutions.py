eb1844e5c26ee1bc2f8f03ba80131f6cc615df4b
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2023-02-14 13:00:26 +0200

    Update HakcerRank Solutions.py

--- HakcerRank Solutions.py
+++ HakcerRank Solutions.py
@@ -1,10 +1,16 @@
-# Migratory Birds Problem 
+# Migratory Birds Solution 
 def migratoryBirds(arr):
     # Write your code here
     count = [0] * len(arr)
     for i in arr:
         count[i] += 1
     return count.index(max(count))
+
+
+# Drawing Book Solution
+def pageCount(n, p):
+    # Write your code here
+    return min(p//2,(n//2)-(p//2))
 
 
 
